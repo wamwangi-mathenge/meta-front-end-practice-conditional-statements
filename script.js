78de69f90ebf4c97d56838e2e9eb32f20e723eff
Brian <wamwangimathenge@gmail.com>
2023-07-20 13:06:36 +0300

    Update the output calls for different days
    This commit updates the output calls with different activities for
    each day

--- script.js
+++ script.js
@@ -11,17 +11,29 @@ if(age >= 65) {
 }
 
 
-var day = "Maandamanz";
+var day = "Friday";
 
 switch(day) {
     case "Monday":
+        console.log("Read a book")
+        break;
     case "Tuesday":
+        console.log("Watch a movie")
+        break;
     case "Wednesday":
+        console.log("Read a book")
+        break;
     case "Thursday":
+        console.log("Play basketball")
+        break;
     case "Friday":
+        console.log("Socialize")
+        break;
     case "Saturday":
+        console.log("Chill")
+        break;
     case "Sunday":
-        console.log("Do something")
+        console.log("Have barbecue")
         break;
     default:
         console.log("There is no such day")
